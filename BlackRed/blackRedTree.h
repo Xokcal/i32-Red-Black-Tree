618b06bf9d6935336e7600f5da999b1102d517e0
Xokcal <2643299260@qq.com>
2026-05-15 10:02:07 +0800

    更新add_fixup和README

---
+++ BlackRed/blackRedTree.h
@@ -0,0 +1,61 @@
+//
+// Created by 26432 on 2026/5/12.
+//
+
+#ifndef I32_BLACK_RED_BLACKREDTREE_H
+#define I32_BLACK_RED_BLACKREDTREE_H
+
+#include "typedef.h"
+#include <stdio.h>
+
+typedef enum {
+    RED,
+    BLACK
+}Color;
+
+typedef struct RBNode{
+    i32 v;
+    Color color;
+
+    struct RBNode* parent;
+    struct RBNode* left;
+    struct RBNode* right;
+}RBNode;
+
+typedef struct RBTree{
+    RBNode* root;
+    RBNode* NIL;
+}RBTree;
+
+RBNode *rb_i32_create_new_NIL();
+
+RBTree *rb_i32_create_new_root();
+
+RBNode *rb_i32_create_new_node(RBTree *root , i32 v);
+
+void rb_i32_print(RBTree* root);
+
+void rb_i32_print_impl(RBTree* root , RBNode *h);
+
+void rb_i32_add(RBTree *root, i32 v);
+
+void rb_i32_left_rotate(RBTree *root, RBNode *h);
+
+void rb_i32_right_rotate(RBTree *root, RBNode *h);
+
+void rb_i32_fixup(RBTree *root, RBNode *new);
+
+void rb_i32_del(RBTree* root , i32 v);
+
+RBNode *rb_i32_min(RBTree *root, RBNode *h);
+
+void rb_i32_del_fixup(RBTree *root, RBNode *delT , RBNode* parent);
+
+//test
+void rb_i32_del_test(RBTree *root, i32 v);
+
+void rb_i32_left_rotate(RBTree *root, RBNode *h);
+
+void add_fixup_test(RBTree* root , RBNode* new);
+
+#endif //I32_BLACK_RED_BLACKREDTREE_H
---
+++ BlackRed/main.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include "typedef.h"
+#include "blackRedTree.h"
+
+int main() {
+    RBTree *root = rb_i32_create_new_root();
+    rb_i32_add(root , 12);
+    rb_i32_add(root , 13);
+    rb_i32_add(root , 21);
+    rb_i32_add(root , 63);
+    rb_i32_add(root , 65);
+    rb_i32_add(root , 82);
+    rb_i32_add(root , 8);
+    rb_i32_add(root , 7);
+    rb_i32_add(root , 72);
+    rb_i32_add(root , 45);
+    rb_i32_add(root , 324);
+    rb_i32_add(root , 24);
+    rb_i32_add(root , 765);
+    rb_i32_add(root , 457);
+    rb_i32_add(root , 257);
+    rb_i32_add(root , 6247);
+    rb_i32_add(root , 4573);
+    rb_i32_add(root , 87);
+    rb_i32_add(root , 875);
+    rb_i32_add(root , 4352);
+    rb_i32_add(root , 753);
+    rb_i32_add(root , 236);
+    rb_i32_add(root , 243);
+    rb_i32_add(root , 8654);
+    rb_i32_add(root , 8658);
+    rb_i32_add(root , 733);
+    rb_i32_add(root , 755);
+    rb_i32_add(root , 1111);
+
+    rb_i32_del_test(root , 4573);
+    rb_i32_del_test(root , 6247);
+    rb_i32_del_test(root , 4352);
+    rb_i32_del_test(root , 8658);
+    rb_i32_del_test(root , 8654);
+
+
+
+    rb_i32_print(root);
+    printf("\nSUCCESS!!");
+    RBNode *min = rb_i32_min(root , root->root->right->right);
+    printf("\n%d\n" , min->v);
+
+    return 0;
+}
